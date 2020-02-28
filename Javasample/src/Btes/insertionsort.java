0e96246ca2db5b9c813291f32d41c73da39e27ad
AyushAssg1 <b20frontend@gmail.com>
2020-02-28 17:43:57 +0530

    MYFIRSTCOMMIT

---
+++ Javasample/src/Btes/insertionsort.java
@@ -0,0 +1,25 @@
+package Btes;
+
+import java.util.Scanner;
+
+public class insertionsort {
+
+	public static void main(String[] args) {
+		int i,j,temp;
+		Scanner s=new Scanner(System.in);
+		int []arr=new int[5];
+		for(j=0;j<arr.length;j++)
+		{
+			arr[j]=s.nextInt();
+			for(i=0;i<arr.length-1;i++)
+			{
+				if(arr[j]>arr[i])
+				{
+					temp=arr[];
+				}
+			}
+		}
+
+	}
+
+}
---
+++ Javasample/src/Btes/nehal.java
@@ -0,0 +1,17 @@
+package Btes;
+
+public class nehal {
+
+	public static void main(String[] args) {
+		int n=1;
+		if(n>=80 && n<=100) {
+			System.out.println("nehal ke number hain");
+		}
+		else if(n>=50 && n<80) {
+			System.out.println("anshul ke number hain ");
+	}
+		else 
+		System.out.println("qamar sala fail hai");
+
+}
+}
